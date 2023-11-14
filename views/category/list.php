698fb122b8e0cf4ccaa24c455b35316695c77f6e
doniyorov.uz <doniyorovrozimurod1@gmail.com>
2023-11-14 18:09:23 +0500

    list

--- views/category/list.php
+++ views/category/list.php
@@ -3,6 +3,30 @@
 
 /** @var \controllers\ProductController $list */
 
-foreach ($list as $item) {
-    echo $item['id'] . $item['name']  . '<br>';
-}
+?>
+
+<div class="container">
+    <a href="/admin/add_category.php" class="btn btn-success"> Qo'shish</a>
+
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>id</th>
+            <th>Category name</th>
+            <th>#</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach (getCategoryList($list) as $item) {
+            echo "<tr>";
+            echo "<td>" . $item['id'] . "</td>";
+            echo "<td>" . $item['name'] . "</td>";
+            echo "<td><a href='/admin/update-categroy.php?.id=". $item["id"];
+        }
+
+        ?>
+        </tbody>
+    </table>
+
+</div>
